7b428cd00df4df1ba59dd423dc31474fa1935f55
Mauricio Rojas <mrojasb2000@gmail.com>
2025-03-09 17:55:40 -0300

    refactor: change implements Rot13

--- 02ComplexProgram/encryptor/rot13.rs
+++ 02ComplexProgram/encryptor/rot13.rs
@@ -2,16 +2,17 @@ pub struct Rot13(pub String);
 
 impl super::Encryptable for Rot13 {
     fn encrypt(&self) -> String {
-        let mut new_string = String::new();
-	for ch in self.0.chars() {
-            if (ch >= 'a' && ch < 'n') || (ch >= 'A' && ch < 'N') {
-               new_string.push((ch as u8 + 13) as char); 
-            } else if (ch >= 'n' && ch < 'z') || (ch >= 'N' && ch < 'Z') {
-               new_string.push((ch as u8 - 13) as char);
-            } else {
-               new_string.push(ch);
-            }
-        }
-        new_string
+        self.0
+            .chars()
+            .map(|ch| {
+                if (ch >= 'a' && ch < 'n') || (ch >= 'A' && ch < 'N') {
+                    (ch as u8 + 13) as char
+                } else if (ch >= 'n' && ch < 'z') || (ch >= 'N' && ch < 'Z') {
+                    (ch as u8 - 13) as char
+                } else {
+                    ch
+                }
+            })
+            .collect()
     }
 }
